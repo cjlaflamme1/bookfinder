dd37701a302bf24a38f44e69078964c75c37f0b3
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-11 16:45:11 -0500

    google books search APIs ready

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import BookDisplay from '../components/bookDisplay';
+import API from '../utils/api';
 
 function Search() {
+    const searchInputRef = useRef();
+    const searchByRef = useRef();
 
+
+    const submitSearch = (event) => {
+        event.preventDefault();
+        if(searchByRef === "Title") {
+            API.searchByTitle(searchInputRef.current.value).then((res) => {
+                console.log(res);
+            }).catch(err => console.log(err));
+        } else {
+            API.searchByAuthor(searchInputRef.current.value).then((res) => {
+                console.log(res);
+            }).catch(err => console.log(err));
+        }
+
+        // return;
+    }
 
     return (
         <>
@@ -10,15 +28,15 @@ function Search() {
                 <h4>
                     Book Search
             </h4>
-                <form>
-                    <input />
+                <form onSubmit={submitSearch}>
+                    <input name="searchContent" ref= {searchInputRef}/>
                     <br />
                     <label for="searchMethod">Search by:</label>
-                    <select name="searchMethod" id="searchMethod">
-                        <option value="Title">Title</option>
-                        <option value="Author">Author</option>
+                    <select name="searchMethod" id="searchMethod" ref={searchByRef}>
+                        <option value="Title" >Title</option>
+                        <option value="Author" >Author</option>
                     </select>
-                    <button>Search</button>
+                    <button type="submit">Search</button>
                 </form>
             </div>
             <div className="books-container">

c3ea6c5be017256d3991c11f71a33c374603d6c6
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-11 12:06:26 -0500

    database connected

---
+++ routes/routesIndex.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const router = require('express').Router();
+// const apiRoutes = require('./api'); be sure to actually connect to index page
+
+// Define API routes here
+// router.use('/api', apiRoutes);
+// Send every other request to the React app
+// Define any API routes before this runs
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const path = require("path");
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const routes = require('./routes/routesIndex');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,13 +15,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+mongoose.connect(process.env.MONGODB_URI);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);

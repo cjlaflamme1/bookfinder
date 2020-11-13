3b10fc9e65ee48b566c3baabd9343413699eeca9
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-13 18:56:52 -0500

    basic book display structure.

--- client/src/components/bookDisplay.js
+++ client/src/components/bookDisplay.js
@@ -9,7 +9,11 @@ function BookDisplay(props) {
     return(
         <>
         <div className="single-book-display">
-
+            <img className="book-image" />
+            <div className="book-title"></div>
+            <div className="book-author"></div>
+            <div className="book-description"></div>
+            <a className="book-link" href="#">Learn More Here</a>
         </div>
         </>
     )

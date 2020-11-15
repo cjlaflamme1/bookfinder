8eb789b946c1005283ba6673c4b70d8f528627e8
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-14 21:43:13 -0500

    serve static files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 require('dotenv').config();
 const mongoose = require('mongoose');
 const routes = require('./routes/routesIndex');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -16,6 +17,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
 
 mongoose.connect(process.env.MONGODB_URI);
 

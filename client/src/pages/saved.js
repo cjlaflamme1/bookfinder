0a47e6b958cb76f578ade1dda0a56c85a3e8aa41
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-15 00:04:40 -0500

    more logs

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -15,7 +15,7 @@ function Saved() {
 
     const submitDelete = (event) => {
         event.preventDefault();
-        
+        console.log('buttons pressed');
         const bookID = event.target.childNodes[0].dataset.id;
         console.log(`trying to delete ${bookID}`);
         API.deleteSaved(bookID);
